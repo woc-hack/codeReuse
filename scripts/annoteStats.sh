2d96946600166d4c38040fe782f0486ee4deffb1
Mahmoud Jahanshahi <mahmoud.jahanshahi@gmail.com>
2021-08-17 16:52:53 -0400

    bug fix

--- scripts/annoteStats.sh
+++ scripts/annoteStats.sh
@@ -17,24 +17,46 @@ for i in {2,3,4,7,8,9,13,14,16,17,18,21,22,23}; do
                 zcat /da5_data/basemaps/gz/annote$j.gz |
                 cut -d\; -f"$i" |
 		~/lookup/lsort 10G -n |
-		uniq -c > data/annoteStats/$i.$j.hist;
+		uniq -c |
+		awk '{if (NF==1) print $1" -1"; else print }' \
+		> data/annoteStats/split/$i.$j.hist;
         done;
 done
 ###merging results
 recJoin() {
 	if [ $# -eq 1 ]; then
-		join -1 1 -2 2 -a1 -a2 - <(cat "$1" | sort -k2,2);
+		join -1 1 -2 2 -a1 -a2 - <(cat "$1" | ~/lookup/lsort 10G -k2,2);
 	else
 		f=$1;
 		shift;
-		join -1 1 -2 2 -a1 -a2 - <(cat "$f" | sort -k2,2) | recJoin "$@";
+		join -1 1 -2 2 -a1 -a2 - <(cat "$f" | ~/lookup/lsort 10G -k2,2) | recJoin "$@";
 	fi;
 };
 for i in {2,3,4,7,8,9,13,14,16,17,18,21,22,23}; do
 	join -1 2 -2 2 -a1 -a2 \
-		<(cat data/annoteStats/$i.0.hist | sort -k2,2) \
-		<(cat data/annoteStats/$i.1.hist | sort -k2,2) |
-	recJoin $(for j in {2..31};do echo "data/annoteStats/$i.$j.hist";done) \
-	> $i.hist
+		<(cat data/annoteStats/split/$i.0.hist | ~/lookup/lsort 10G -k2,2) \
+		<(cat data/annoteStats/split/$i.1.hist | ~/lookup/lsort 10G -k2,2) |
+	recJoin $(for j in {2..31};do echo "data/annoteStats/split/$i.$j.hist";done) |
+	awk '{sum=0;for(i=2; i<=NF; i++) {sum+=$i} print $1";"sum}' | 
+	~/lookup/lsort 10G -t\; -k1,1 -n > data/annoteStats/$i.hist;
 done
+
+#######
+n=13;
+for i in {0..31}; do
+	cat data/annoteStats/$n.$i.hist |
+	awk '{if (NF==1) print $1" -1"; else print }' > tmp;
+	mv tmp data/annoteStats/$n.$i.hist;
+done
+#
+n=18;
+join -1 2 -2 2 -a1 -a2 \
+                <(cat data/annoteStats/$n.0.hist | ~/lookup/lsort 10G -k2,2) \
+                <(cat data/annoteStats/$n.1.hist | ~/lookup/lsort 10G -k2,2) |
+recJoin $(for j in {2..31};do echo "data/annoteStats/$n.$j.hist";done) |
+awk '{sum=0;for(i=2; i<=NF; i++) {sum+=$i} print $1";"sum}' |
+~/lookup/lsort 10G -t\; -k1,1 -n > data/annoteStats/$n.hist;
+#
+n=13;
+mv data/annoteStats/$n.*.hist data/annoteStats/split/ 
 
