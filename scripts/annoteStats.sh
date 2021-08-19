d132dc71d8e7fd36bd0d5a7a70d1d460eca67a50
Mahmoud Jahanshahi <mahmoud.jahanshahi@gmail.com>
2021-08-19 09:38:33 -0400

    removed old scripts

--- scripts/annoteStats.sh
+++ scripts/annoteStats.sh
@@ -2,16 +2,6 @@
 
 #hist
 ##2$nmc;3$nblob;4$nsa;7$ncore;8$ncmt;9$na;13$cPr;14$cST;16$nmcb;17$nblobb;18$nsb;21$ncoreb;22$ncmtb;23$nab
-###v1 - normal sort
-for i in {2,3,4,7,8,9,13,14,16,17,18,21,22,23}; do
-	for j in {0..31}; do
-        	zcat /da5_data/basemaps/gz/annote$j.gz |
-		cut -d\; -f"$i";
-	done |
-	sort -n | 
-	uniq -c > data/annoteStats/$i.hist;
-done
-###v2 - using lsort
 for i in {2,3,4,7,8,9,13,14,16,17,18,21,22,23}; do
         for j in {0..31}; do
                 zcat /da5_data/basemaps/gz/annote$j.gz |
@@ -22,7 +12,7 @@ for i in {2,3,4,7,8,9,13,14,16,17,18,21,22,23}; do
 		> data/annoteStats/split/$i.$j.hist;
         done;
 done
-###merging results
+##merging results
 recJoin() {
 	if [ $# -eq 1 ]; then
 		join -1 1 -2 2 -a1 -a2 - <(cat "$1" | ~/lookup/lsort 10G -k2,2);
