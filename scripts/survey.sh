cfc4825c96c64c93b087eccc5bc201545d1a68f5
Mahmoud Jahanshahi <mahmoud.jahanshahi@gmail.com>
2021-10-18 17:52:05 -0400

    refined the survey search domain

--- scripts/survey.sh
+++ scripts/survey.sh
@@ -13,15 +13,11 @@ for i in {8..15}; do
 done |
 gzip > ../data/survey/bTb.gz;
 #big to small
-for i in {16..23}; do
-    zcat /da5_data/basemaps/gz/annote$i.gz |
-    awk -F\; '{if ($2>10 && $4>10 && $16<10 && $18==0) print}'
-done |
+zcat /da5_data/basemaps/gz/annote16.gz |
+awk -F\; '{if ($2>10 && $4>10 && $16<10 && $18==0) print}' |
 gzip > ../data/survey/bTs.gz;
 #small to small
-for i in {24..31}; do
-zcat /da5_data/basemaps/gz/annote$i.gz |
-    awk -F\; '{if ($2<10 && $4==0 && $16<10 && $18==0) print}'
-done |
+zcat /da5_data/basemaps/gz/annote24.gz |
+awk -F\; '{if ($2<10 && $4==0 && $16<10 && $18==0) print}' |
 gzip > ../data/survey/sTs.gz;
 
