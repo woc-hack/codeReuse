d84becdfef14449e1777a3a04ff2d7b55e6feca7
Mahmoud Jahanshahi <mahmoud.jahanshahi@gmail.com>
2021-11-01 14:32:24 -0400

    first round survey

--- scripts/survey.sh
+++ scripts/survey.sh
@@ -20,4 +20,52 @@ gzip > ../data/survey/bTs.gz;
 zcat /da5_data/basemaps/gz/annote24.gz |
 awk -F\; '{if ($2<10 && $4==0 && $16<10 && $18==0) print}' |
 gzip > ../data/survey/sTs.gz;
+#random
+zcat /da5_data/basemaps/gz/annote17.gz | 
+head -60000000 |
+gzip >../data/survey/rand.gz;
+
+#initial survey
+for set in sTb bTb bTs sTs rand; do
+    #getting first 1 million entries
+    zcat ../data/survey/$set.gz |
+    head -1000000 > ../data/survey/initial/$set;
+    #upstream-downstream combination counts
+    cat ../data/survey/initial/$set |
+    cut -d\; -f1,15 | 
+    ~/lookup/lsort 10G |
+    uniq -c |
+    ~/lookup/lsort 10G -nr -k1,1 > ../data/survey/initial/$set.counts;
+    #upstream counts
+    cat ../data/survey/initial/$set |
+    cut -d\; -f1 |
+    ~/lookup/lsort 10G |
+    uniq -c | 
+    ~/lookup/lsort 10G -nr -k1,1 > ../data/survey/initial/$set.fcounts;
+    #accumulation
+    cat ../data/survey/initial/$set |
+    while read line; do
+        type=$(echo $line | 
+            cut -d\; -f11 |
+            ~/lookup/getValues -f b2f |
+            sed 's|.*\.||g' |
+            ~/lookup/lsort |
+            uniq -c |
+            ~/lookup/lsort 10G -nr |
+            head -1 |
+            sed 's| *||' |
+            cut -d' ' -f2);
+        ud=$(echo $line | cut -d\; -f1,15);
+        count=$(cat ../data/survey/initial/$set.counts |
+            grep "$ud" |
+            sed 's| *||' | 
+            cut -d' ' -f1);
+        u=$(echo "$ud" | cut -d\; -f1);
+        fcount=$(cat ../data/survey/initial/$set.fcounts |
+            grep "$u" |
+            sed 's| *||' | 
+            cut -d' ' -f1);
+        echo "$line;$type;$count;$fcount";
+    done > ../data/survey/initial/$set.2
+done
 
