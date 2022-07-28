ac77fa2ddb8462bae58333165667da78c128e155
mahmoudjahanshahi <mahmoud.jahanshahi@gmail.com>
2022-07-28 19:14:07 -0400

    s2 candidates

--- scripts/survey2.sh
+++ scripts/survey2.sh
@@ -36,7 +36,8 @@ for set in sTb bTb bTs sTs rand; do
     done;
 done
 
-# initial sample -> 5000000
+# initial sample
+# can 0 -> 5000000
 for set in sTb bTb bTs sTs rand; do
     for i in {0,1}; do
         shuf -n 500000 "data/survey/second/${set}.$i"
@@ -76,23 +77,65 @@ for i in {0..127}; do
         <(zcat /da?_data/basemaps/gz/b2fAFullU"$i".s)
 done |
 gzip >data/survey/second/can.b2tAc.s;
-# filtering user emails
-zcat data/survey/second/can.b2tAc.s | 
-cut -d\; -f1,3 |
-while read -r line; do
-    ((c++));
-    l=$((c%10+1));
-    b=$(echo "$line" | cut -d\; -f1);
-    email=$(echo "$line" | cut -d\; -f2 | sed 's|.* <||;s|>.*||');
-    echo "$b;$email;$(curl -s \
-        -H "Accept: application/vnd.github+json" \
-        -H "Authorization: token $(sed -n ${l}p ~/github_tokens)" \
-        "https://api.github.com/search/users?q=${email}" |
-        jq .total_count)"
-done |
-gzip >data/survey/second/can.b2e1.s;
 #b2ftAc -> 1356261
 LC_ALL=C LANG=C join -t\; \
     <(zcat data/survey/second/can.b2f.s) \
     <(zcat data/survey/second/can.b2tAc.s | ~/lookup/lsort 50G -t\;) |
 gzip >data/survey/second/can.b2ftAc.s;
+# b2ft
+zcat data/survey/second/can.b2ftAc.s |
+cut -d\; -f1-3 |
+awk -F\; '{if ($2!="") print}' \
+>data/survey/second/can.b2ft.s;
+# survey2.ipynb
+# f -> f>10000 , t -> 10 bins
+#b2ftAc.2 -> 1356243
+LC_ALL=C LANG=C join -t\; \
+    data/survey/second/can.b2ft.s \
+    <(zcat data/survey/second/can.b2ftAc.s | cut -d\; -f1,4,5) |
+gzip >data/survey/second/can.b2ftAc.2.s;
+# can 1 -> 3257231
+# 1$key;2$b;3$f;4$t;5$A;6$c;7$uP;8$nsu;9$ncmtu;10$d;11$dP;12$nsd;13$ncmtd
+LC_ALL=C LANG=C join -t\; -2 4\
+    <(zcat data/survey/second/can.b2ftAc.2.s) \
+    <(zcat data/survey/second/can.0 |
+        cut -d\; -f1,4,8,11,12,15,18,22 |
+        ~/lookup/lsort 50G -t\; -k4,4) |
+perl -e 'while(my $l=<STDIN>){$n=$n+1;print "$n;$l"}' |
+gzip >data/survey/second/can.1.bs;
+# delay, star, commit as categorical factor
+zcat data/survey/second/can.1.bs |
+cut -d\; -f1,8,9,10,12,13 >Rtmp;
+# survey2.ipynb
+# d -> "<1" "1< <3" ">3" , stars/commit -> b,m,s
+# can 2 -> 3066342
+# 1$key;2$b;3$f;4$t;5$A;6$c;7$uP;8$dP;9$d;10$s
+LC_ALL=C LANG=C join -t\; \
+    <(zcat data/survey/second/can.1.bs | 
+        cut -d\; -f1-7,11 |
+        ~/lookup/lsort 50G -t\; -k1,1) \
+    <(~/lookup/lsort 50G -t\; -k1,1 <Rtmp) |
+gzip >data/survey/second/can.2.ks;
+# filtering user emails
+# A2a -> 127691
+zcat data/survey/second/can.2.ks | 
+cut -d\; -f5 | 
+~/lookup/lsort 50G -u | 
+while read -r a; do
+    echo "$a;$(echo "$a" | sed 's|.*<||;s|>.*||')"
+done |
+awk -F\; '{if ($2!="") print}' |
+gzip >data/survey/second/can.A2e.s;
+# github api
+zcat data/survey/second/can.A2e.s | 
+cut -d\; -f2 |
+while read -r email; do
+    ((c++));
+    l=$((c%10+1));
+    echo "$email;$(curl -s \
+        -H "Accept: application/vnd.github+json" \
+        -H "Authorization: token $(sed -n ${l}p ~/github_tokens)" \
+        "https://api.github.com/search/users?q=${email}" |
+        jq .total_count)"
+done |
+gzip >data/survey/second/can.e2v.s;
